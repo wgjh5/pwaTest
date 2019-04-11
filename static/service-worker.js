9ca53223f8fce56006e990ec7ae8c962871cb591
wgjh5 <854453495@qq.com>
2019-04-11 21:06:22 +0800

    11

--- static/service-worker.js
+++ static/service-worker.js
@@ -14,20 +14,20 @@ self.addEventListener("fetch", event => {
     const url = new URL(event.request.url);
     console.log('fetch', event.request);
 });
-// 
-// 	if ('serviceWorker' in navigator) {
-// 	    window.addEventListener('load', function () {
-// 	        navigator.serviceWorker.register('/eee/service-worker.js', {scope: '/eee/'})
-// 	            .then(function (registration) {
-// 	
-// 	                // 注册成功
-// 	                console.log('注册成功 ', registration.scope);
-// 	            })
-// 	            .catch(function (err) {
-// 	
-// 	                // 注册失败:(
-// 	                console.log('注册失败', err);
-// 	            });
-// 	    });
-// 	}
+
+	if ('serviceWorker' in navigator) {
+	    window.addEventListener('load', function () {
+	        navigator.serviceWorker.register('/static/service-worker.js', {scope: '/static/'})
+	            .then(function (registration) {
+	
+	                // 注册成功
+	                console.log('注册成功 ', registration.scope);
+	            })
+	            .catch(function (err) {
+	
+	                // 注册失败:(
+	                console.log('注册失败', err);
+	            });
+	    });
+	}
 
